1c85a043cd0040d71c52aaef01d368a7a9d48336
Ignacio <nac0530002@gmail.com>
2025-11-06 14:59:57 -0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -4,7 +4,7 @@ CREATE TABLE barberia(
     nro INT,
     dpto INT,
     hora_apertura TIME,
-    hora_apertura TIME,
+    hora_cierre TIME
 );
 CREATE TABLE barbero(
     nro_legajo INT PRIMARY KEY,
@@ -24,43 +24,43 @@ CREATE TABLE trabajan(
     hora_fin TIME,
     PRIMARY KEY (codigo_barberia, nro_legajo, dia, hora_inicio),
     CONSTRAINT fk_codigo_barberia
-        FOREIGN KEY codigo_barberia 
+        FOREIGN KEY (codigo_barberia) 
         REFERENCES barberia(codigo_barberia),
     CONSTRAINT fk_nro_legajo
-        FOREIGN KEY nro_legajo 
-        REFERENCES barbero(nro_legajo),
+        FOREIGN KEY (nro_legajo) 
+        REFERENCES barbero(nro_legajo)
 );
 CREATE TABLE senior(
     nro_legajo INT PRIMARY KEY,
     anios_experiencia INT,
-    tiene_certificado BOOLEAN DEFAULT FALSE;
+    tiene_certificado BOOLEAN DEFAULT FALSE,
     CONSTRAINT fk_senior_nro_legajo
-        FOREIGN KEY nro_legajo
-        REFERENCES barbero(nro_legajo),
+        FOREIGN KEY (nro_legajo)
+        REFERENCES barbero(nro_legajo)
 );
 CREATE TABLE junior(
     nro_legajo INT PRIMARY KEY,
     nombre_academia VARCHAR(30),
     CONSTRAINT fk_junior_nro_legajo
-        FOREIGN KEY nro_legajo
-        REFERENCES barbero(nro_legajo),
+        FOREIGN KEY (nro_legajo)
+        REFERENCES barbero(nro_legajo)
 );
 CREATE TABLE servicio(
     codigo_servicio INT PRIMARY KEY,
     nombre_servicio VARCHAR(40),
     duracion INT,
-    precio_servicio INT,
+    precio_servicio INT
 );
 CREATE TABLE brinda_servicio(
     nro_legajo INT,
     codigo_servicio INT,
-    PRIMARY KEY(nro_legajo, codigo_barberia),
+    PRIMARY KEY(nro_legajo, codigo_servicio),
     CONSTRAINT fk_servicio_nro_legajo
-        FOREIGN KEY nro_legajo
+        FOREIGN KEY (nro_legajo)
         REFERENCES barbero(nro_legajo),
     CONSTRAINT fk_codigo_servicio
-        FOREIGN KEY codigo_servicio
-        REFERENCES servicio(codigo_servicio),
+        FOREIGN KEY (codigo_servicio)
+        REFERENCES servicio(codigo_servicio)
 );
 CREATE TABLE clientes(
     num_cliente INT PRIMARY KEY,
@@ -74,9 +74,9 @@ CREATE TABLE clientes(
 CREATE TABLE telefono_cliente(
     num_cliente INT,
     telefono_cliente varchar(10),
-    PRIMARY KEY (num_cliente, telefono_cliente)
+    PRIMARY KEY (num_cliente, telefono_cliente),
     CONSTRAINT fk_telefono_cliente
-        FOREIGN KEY num_cliente
+        FOREIGN KEY (num_cliente)
         REFERENCES clientes(num_cliente)
 );
 CREATE TABLE turno(
@@ -85,44 +85,49 @@ CREATE TABLE turno(
     fecha_turno DATE, 
     hora_turno TIME, 
     codigo_barberia INT, 
-    nro_legajo INT
+    nro_legajo INT,
     CONSTRAINT fk_turno_barberia
-        FOREIGN KEY codigo_barberia
+        FOREIGN KEY (codigo_barberia)
         REFERENCES barberia(codigo_barberia),
     CONSTRAINT fk_turno_cliente
-        FOREIGN KEY num_cliente
+        FOREIGN KEY (num_cliente)
         REFERENCES clientes(num_cliente),
     CONSTRAINT fk_turno_barbero
-        FOREIGN KEY nro_legajo
-        REFERENCES barbero(nro_legajoe),
+        FOREIGN KEY (nro_legajo)
+        REFERENCES barbero(nro_legajo)
 
 );
 CREATE TABLE se_realiza(
-    id_turno INT PRIMARY KEY,
-    nro_legajo INT PRIMARY KEY, 
-    codigo_servicio INT PRIMARY KEY, 
+    id_turno INT,
+    nro_legajo INT, 
+    codigo_servicio INT, 
     precio_final INT,
+	PRIMARY KEY(id_turno, nro_legajo, codigo_servicio),
     CONSTRAINT fk_turno_realiza
-        FOREIGN KEY id_turno
-        REFERENCES turno(id_turno)
+        FOREIGN KEY (id_turno)
+        REFERENCES turno(id_turno),
     CONSTRAINT fk_realiza_barberia
-        FOREIGN KEY codigo_barberia
-        REFERENCES barberia(codigo_barberia),
+        FOREIGN KEY (codigo_servicio)
+        REFERENCES servicio(codigo_servicio),
     CONSTRAINT fk_realiza_barbero
-        FOREIGN KEY nro_legajo
-        REFERENCES barbero(nro_legajo),
+        FOREIGN KEY (nro_legajo)
+        REFERENCES barbero(nro_legajo)
+);
+CREATE TABLE medio_pago(
+    id_medio_pago INT PRIMARY KEY, 
+    nombre_medio_pago VARCHAR(50)
 );
 CREATE TABLE pago(
     id_pago INT PRIMARY KEY, 
     monto_total INT, 
     fecha_pago DATE, 
     id_turno INT, 
-    id_medio_pago INT
+    id_medio_pago INT,
     CONSTRAINT fk_pago_turno
-        FOREIGN KEY id_turno
-        REFERENCES turno(id_turno)
+        FOREIGN KEY (id_turno)
+        REFERENCES turno(id_turno),
     CONSTRAINT fk_pago_medio
-        FOREIGN KEY id_medio_pago
+        FOREIGN KEY (id_medio_pago)
         REFERENCES medio_pago(id_medio_pago)
 );
 CREATE TABLE membresia(
@@ -132,7 +137,7 @@ CREATE TABLE membresia(
     costo_mensual INT, 
     codigo_servicio INT,
     CONSTRAINT fk_membresia_servicio
-        FOREIGN KEY codigo_servicio
+        FOREIGN KEY (codigo_servicio)
         REFERENCES servicio(codigo_servicio)
 );
 CREATE TABLE encuesta(
@@ -143,13 +148,10 @@ CREATE TABLE encuesta(
     id_turno INT
 
 );
-CREATE TABLE medio_pago(
-    id_medio_pago INT PRIMARY KEY, 
-    nombre_medio_pago VARCHAR(50)
-);
+
 CREATE TABLE telefono_barberia(
     odigo_barberia INT PRIMARY KEY, 
-    telefono_barberia VARCHAR(20),
+    telefono_barberia VARCHAR(20)
 );
 
 
